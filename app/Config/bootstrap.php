9e2f7a9c04e77765b130e1a85261e9f1437a7c1f
Luis Dias <luis.edudias@gmail.com>
2013-01-24 16:29:29 +0000

    minor: configuration settings for cakephp 2.2.x+

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -22,6 +22,10 @@
  * @since         CakePHP(tm) v 0.10.8.2117
  * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
+Configure::write('Dispatcher.filters', array(
+    'AssetDispatcher',
+    'CacheDispatcher'
+));
 Configure::write('Config.language', 'eng');
 // Setup a 'default' cache configuration for use in the application.
 Cache::config('default', array('engine' => 'File'));
